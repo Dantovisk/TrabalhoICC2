2c15adb2c9810a94c7aa15b246f2072ea135f08d
João Gabriel Pieroli da Silva <joaogabrielpieroli18@gmail.com>
2024-10-27 01:02:09 -0300

    Update Trabalho1.c
    Falta arrumar a função remover, ela continua um pouco bugada

--- Trabalho1.c
+++ Trabalho1.c
@@ -5,37 +5,88 @@
 
 #define MAX_DESCR 100
 #define MAXN 1000
+#define INF 100000007  
 
-/* Armazena um horário de chegada */
+/* Armazena um horário de chegada 
+hh: horas
+mm: minutos
+ss: segundos
+*/
 typedef struct {
     int hh;
     int mm;
     int ss;
 } HORARIO;
 
-/* Armazena informações de um processo */
+/* Armazena informações de um processo 
+prior: número na fila de prioridade
+valor: variável auxiliar com o horário concatenado
+chegada: struct que guarda o horário em campos (horas, minutos e segundos)
+descricao[MAX_DESCR]: descrição do processo
+*/
 typedef struct {
     int prior;
+    int valor;
     HORARIO chegada;
     char descricao[MAX_DESCR];
-
-    // variavel auxiliar que concatena os tempos de chegada
-    // dessa forma será muito mais fácil ordenar os elementos
-    int valor;
 } CELULA;
 
-/* Lista para lidar com os processos */
+/* Lista para lidar com os processos
+tam: quantidade de elementos
+celulaPrior: uma célula ordenada pela prioridade
+celulaTempo: uma célula ordenada pelo tempo 
+*/
 typedef struct {
     int tam;
     CELULA *celulaPrior [MAXN];
     CELULA *celulaTempo [MAXN];
 } PROCESSOS;
 
-// Quick Sort com celular
+/* A função realiza a cópia das informações de uma célula para outra */
+void copiaCelula(CELULA *origem, CELULA *destino) {
+    if(origem != NULL && destino!=NULL) {
+        strcpy(destino->descricao, origem->descricao);
+        destino->prior = origem->prior;
+        destino->valor = origem->valor;
+        destino->chegada.hh = origem->chegada.hh;
+        destino->chegada.mm = origem->chegada.mm;
+        destino->chegada.ss = origem->chegada.ss;
+    }
+}
+/* A função inicializa as células com valores absurdos para ser possível*/
+void inicializaBestCelula(CELULA *celula) {
+    celula->prior=-1;
+    celula->valor=INF;
+    memset(celula->descricao, 0, sizeof(celula->descricao));
+    celula->chegada = (HORARIO) {INF, INF, INF};
+    return;
+}
+
+/* A função faz a alocação dinâmica da memória utilizada para a struct CELULA, já recebendo do usuário os valores que serão
+inicializados nos campos da struct, sejam eles, a prioridade, o horário e fazendo a conversão do valor.
+*/
+CELULA *criarCelula(CELULA *bestPrior, CELULA *bestTempo){
+    CELULA *c = (CELULA *) malloc(sizeof(CELULA));
+    if(c != NULL){
+        scanf("%d %d:%d:%d %s", &c->prior, &c->chegada.hh, &c->chegada.mm, &c->chegada.ss, c->descricao);
+        c->valor = (c->chegada.hh << 20) + (c->chegada.mm << 10) + c->chegada.ss;
+        if(c->prior > bestPrior->prior) {
+            copiaCelula(c, bestPrior);
+        }
+        if(c->valor < bestTempo->valor) {
+            copiaCelula(c, bestTempo);
+        }
+    }
+    return c;
+}
+
+/* A função faz um Quicksort recursivamente, a fim de tornar o vetor ordenado em O(n log n)
+*/
 void quickSort(CELULA *vet[], int inicio, int fim, bool isPrior){
     int i = inicio;
     int j = fim;
-    CELULA *pivo = vet[(i + j)/2]; // pivo medio, pode ser usado mediana de 3
+    // Pivô medio
+    CELULA *pivo = vet[(i + j)/2]; 
     
     while(i <= j){
         if (isPrior)
@@ -64,193 +115,227 @@ void quickSort(CELULA *vet[], int inicio, int fim, bool isPrior){
     return;
 }
 
-//começando do fim da lista, vai deslocando todos os elementos
-//um índice à direita até queencontre a posição ideal para inserir
-void insereOrdenado(CELULA *c1, PROCESSOS *terminal){
-    if(terminal->tam != MAXN){
+/* A função faz a inserção da célula, sem se importar com ordenação, apenas se insere no próximo espaço vago
+*/
+void inserir(CELULA *c1, PROCESSOS *terminal){
+    if(terminal->tam < MAXN){
         int i;
-        for(i = terminal->tam - 1; terminal->celulaPrior[i]->prior > c1->prior && i >= 0; i--)
-            terminal->celulaPrior[i + 1] = terminal->celulaPrior[i];
-
-        terminal->celulaPrior[i + 1] = c1;
-
-        for(i = terminal->tam - 1; terminal->celulaTempo[i]->valor < c1->valor && i >= 0; i--)
-            terminal->celulaTempo[i + 1] = terminal->celulaTempo[i];
-
-        terminal->celulaTempo[i + 1] = c1;
-
+        terminal->celulaTempo[terminal->tam] = c1;
+        terminal->celulaPrior[terminal->tam] = c1;
         terminal->tam++;
     }
 
     return; 
 }
 
-// começando do fim da lista, vai deslocando todos os elementos
-// um índice à esquerda até que chegue no elemento a ser removido
-void removeCelula(PROCESSOS *terminal){
+void removeCelula(PROCESSOS *terminal, CELULA *bestPrior, CELULA *bestTempo){
     char key;
     scanf(" -%c", &key);
+    if (terminal->tam == 0) return;
 
-    if(terminal->tam != 0){
-        if(key == 'p'){
-            for(int i = 0; i < terminal->tam; i++){
-                if(terminal->celulaTempo[i] == terminal->celulaPrior[terminal->tam - 1])
-                    for(int j = i; j < terminal->tam - 1; j++)
-                        terminal->celulaTempo[j] = terminal->celulaTempo[j + 1];
+
+    if (key == 'p') {
+        // ENCONTRAR A CÉLULA COM A MAIOR PRIORIDADE
+        int segundomelhor=-1;
+        int indice = 0;
+        for (int i = 0; i < terminal->tam; i++) {
+            if (terminal->celulaPrior[i]->prior == bestPrior->prior) {
+                free(terminal->celulaPrior[i]);
+                terminal->celulaPrior[i] = NULL;
+                for (int j = i; j < terminal->tam-1; j++) {
+                    terminal->celulaPrior[j] = terminal->celulaPrior[j + 1];
+                }
             }
-            free(terminal->celulaPrior[terminal->tam - 1]);
-            terminal->celulaPrior[terminal->tam - 1] = NULL;
+            if (terminal->celulaTempo[i]->prior == bestPrior->prior) {
+                free(terminal->celulaTempo[i]);
+                terminal->celulaTempo[i] = NULL;
+                for (int j = i; j < terminal->tam-1; j++) {
+                    terminal->celulaTempo[j] = terminal->celulaTempo[j + 1];
+                }
+            } else if (segundomelhor < terminal->celulaTempo[i]->prior) {
+                segundomelhor = terminal->celulaTempo[i]->prior;
+                indice = i;
+            }
         }
-        else if(key == 't'){
-            for(int i = 0; i < terminal->tam; i++){
-                if(terminal->celulaPrior[i] == terminal->celulaTempo[terminal->tam - 1])
-                    for(int j = i; j < terminal->tam - 1; j++)
-                        terminal->celulaPrior[j] = terminal->celulaPrior[j + 1];
+        if (terminal->tam > 0) {
+            copiaCelula(terminal->celulaTempo[indice], bestPrior);
+        } else {
+            inicializaBestCelula(bestPrior);
+        }
+
+    } else if (key == 't') {
+        // ENCONTRAR A CÉLULA COM O MENOR TEMPO
+        int segundomelhor=INF;
+        int indice = 0;
+        for (int i = 0; i < terminal->tam; i++) {
+            if (terminal->celulaTempo[i]->valor == bestTempo->valor) {
+                free(terminal->celulaTempo[i]);
+                terminal->celulaTempo[i] = NULL;
+                for (int j = i; j < terminal->tam-1; j++) {
+                    terminal->celulaTempo[j] = terminal->celulaTempo[j + 1];
+                }
             }
-            free(terminal->celulaTempo[terminal->tam - 1]);
-            terminal->celulaTempo[terminal->tam - 1] = NULL;
+            if (terminal->celulaPrior[i]->valor == bestTempo->valor) {
+                free(terminal->celulaPrior[i]);
+                terminal->celulaPrior[i] = NULL;
+                for (int j = i; j < terminal->tam-1; j++) {
+                    terminal->celulaPrior[j] = terminal->celulaPrior[j + 1];
+                }
+            } else if (segundomelhor > terminal->celulaPrior[i]->valor) {
+                segundomelhor = terminal->celulaPrior[i]->valor;
+                indice = i;
+            }
+        }
+        if (terminal->tam > 0) {
+            copiaCelula(terminal->celulaPrior[indice], bestTempo);
+        } else {
+            inicializaBestCelula(bestTempo);
         }
     }
-
     terminal->tam--;
-    return;
 }
 
-CELULA *criarCelula(void){
-    CELULA *c = (CELULA *) malloc(sizeof(CELULA));
-    if(c != NULL){
-        scanf("%d %d:%d:%d %s", &c->prior, &c->chegada.hh, &c->chegada.mm, &c->chegada.ss, c->descricao);
-        c->valor = (c->chegada.hh << 20) + (c->chegada.mm << 10) + c->chegada.ss;
-    }
 
-    return c;
-}
 
-// VAI TER QUE FAZER BUSCA BINARIA SE PA!
-void mudarCelula(PROCESSOS *terminal){
+/* A função faz a mudança das informações da célula e, caso ela seja melhor que as outras em tempo e/ou prioridade, troca a vigente
+*/
+void mudarCelula(PROCESSOS *terminal, CELULA *bestPrior, CELULA *bestTempo){
     char key;
     scanf(" -%c", &key);
 
+    bool melhor = false;
     int i;
+
     if(key == 'p'){
+
         int prior_old, prior_new;
         scanf("%d|%d", &prior_old, &prior_new);
 
+        // VERIFICA SE A MUDANÇA DEVE CAUSAR ALTERAÇÃO DO MELHOR
+        if(prior_new > bestPrior->prior) {
+            melhor = true;
+        }
+
         for(i = 0; i < terminal->tam; i++){
+            // TROCAR EM PRIOR
             if(terminal->celulaPrior[i]->prior == prior_old){
                 terminal->celulaPrior[i]->prior = prior_new;
-                break;
+                // TROCAR A MELHOR VIGENTE EM PRIOR PELA NOVA MELHOR
+                if(melhor == true) {
+                    copiaCelula(terminal->celulaPrior[i], bestPrior);
+                }
+            }
+            // TROCAR EM TEMPO
+            if(terminal->celulaTempo[i]->prior == prior_old){
+                terminal->celulaTempo[i]->prior = prior_new;
             }
         }
     } else if(key == 't'){
-        int hh_old, mm_old, ss_old, hh_new, valor_old;
+
+        int hh_old, mm_old, ss_old, valor_new, valor_old;
         HORARIO new;
         scanf("%d:%d:%d|%d:%d:%d", &hh_old, &mm_old, &ss_old, &new.hh, &new.mm, &new.ss);
         valor_old = (hh_old << 20) + (mm_old << 10) + ss_old;
-        
-        for(i = 0; i < terminal->tam; i++){
+        valor_new = (new.hh << 20) + (new.mm << 10) + new.ss;
+
+        // VERIFICA SE A MUDANÇA DEVE CAUSAR ALTERAÇÃO DO MELHOR
+        if(valor_new < valor_old) {
+            melhor = true;
+        } 
+
+        for(i = 0; i < terminal->tam; i++) {
+            // TROCAR EM TEMPO
             if(terminal->celulaTempo[i]->valor == valor_old){
                 terminal->celulaTempo[i]->chegada = new;
-                terminal->celulaTempo[i]->valor = (new.hh << 20) + (new.mm << 10) + new.ss;
-                break;
+                terminal->celulaTempo[i]->valor = valor_new;
+                // TROCAR A MELHOR VIGENTE EM TEMPO PELA NOVA MELHOR
+                if(melhor == true) {
+                    copiaCelula(terminal->celulaTempo[i], bestTempo);
+                }
+            }
+            // TROCAR EM PRIOR
+            if(terminal->celulaPrior[i]->valor == valor_old){
+                terminal->celulaPrior[i]->chegada = new;
+                terminal->celulaPrior[i]->valor = valor_new;
             }
         }
     }
-
     return;
 }
 
+/* A função printa todos os processos, com a prioridade, o horário e a descrição
+*/
 void printarProcessos(CELULA *celulas[], int n){
     for(int i = n-1; i >= 0; i--){
-        printf("%02d %02d:%02d:%02d %s\n", celulas[i]->prior, celulas[i]->chegada.hh, 
-        celulas[i]->chegada.mm, celulas[i]->chegada.ss, celulas[i]->descricao);
+        printf("%02d %02d:%02d:%02d %s\n", celulas[i]->prior, celulas[i]->chegada.hh, celulas[i]->chegada.mm, celulas[i]->chegada.ss, celulas[i]->descricao);
     }
+    printf("\n");
     
 }
 
 int main(){
-    bool quickViavel = true; // O quickSort é feito somente no primeiro print.
     char keyword[10];
-    PROCESSOS terminal1;
-    terminal1.tam = 0;
+
+    PROCESSOS terminal;
+    terminal.tam = 0;
+
+    CELULA bestPrior;
+    CELULA bestTempo;
+    inicializaBestCelula(&bestPrior);
+    inicializaBestCelula(&bestTempo);
     
     do{
         scanf("%s", keyword);
 
         // Comando "add".
         if(strcmp(keyword, "add") == 0){
-            CELULA *c1 = criarCelula();
-
-            if(quickViavel){
-                terminal1.celulaPrior[terminal1.tam] = c1;
-                terminal1.celulaTempo[terminal1.tam] = c1;
-                terminal1.tam++;
-            }
-            else //caso ja tenha realizado o quicksort
-                insereOrdenado(c1, &terminal1);
+            CELULA *c1 = criarCelula(&bestPrior, &bestTempo);
+            inserir(c1, &terminal);
         }
 
         // Comando "exec"
         else if(strcmp(keyword, "exec") == 0){
-            if(quickViavel){
-                quickSort(terminal1.celulaPrior, 0, terminal1.tam-1, true);
-                quickSort(terminal1.celulaTempo, 0, terminal1.tam-1, false);
-                quickViavel = false;
-            }
-
-            removeCelula(&terminal1);
+            removeCelula(&terminal, &bestPrior, &bestTempo);
         }
 
         // Comando "next"
         else if(strcmp(keyword, "next") == 0){
-            if(quickViavel){
-                quickSort(terminal1.celulaPrior, 0, terminal1.tam-1, true);
-                quickSort(terminal1.celulaTempo, 0, terminal1.tam-1, false);
-                quickViavel = false;
-            }
-
             char key;
             scanf(" -%c", &key);
-
-            if(key == 'p')
-                printf("%02d %02d:%02d:%02d %s\n", terminal1.celulaPrior[terminal1.tam - 1]->prior, terminal1.celulaPrior[terminal1.tam - 1]->chegada.hh, 
-                terminal1.celulaPrior[terminal1.tam - 1]->chegada.mm, terminal1.celulaPrior[terminal1.tam - 1]->chegada.ss, terminal1.celulaPrior[terminal1.tam - 1]->descricao);
-            else if(key == 't')
-                printf("%02d %02d:%02d:%02d %s\n", terminal1.celulaTempo[terminal1.tam - 1]->prior, terminal1.celulaTempo[terminal1.tam - 1]->chegada.hh, 
-                terminal1.celulaTempo[terminal1.tam - 1]->chegada.mm, terminal1.celulaTempo[terminal1.tam - 1]->chegada.ss, terminal1.celulaTempo[terminal1.tam - 1]->descricao);
+            if(key == 'p') {
+                if(bestPrior.prior != -1) {
+                    printf("%02d %02d:%02d:%02d %s\n", bestPrior.prior, bestPrior.chegada.hh, bestPrior.chegada.mm, bestPrior.chegada.ss, bestPrior.descricao);
+                }
+            } else if(key == 't') {
+                if(bestTempo.valor != INF) {
+                    printf("%02d %02d:%02d:%02d %s\n", bestTempo.prior, bestTempo.chegada.hh, bestTempo.chegada.mm, bestTempo.chegada.ss, bestTempo.descricao);
+                }
+            }
         }
         
-        // sim dante, esse comando está feito com a bunda, fiquei com preguiça de mover a célula antiga pra posição ordenada, lide com isso!
+        // Comando "change"
         else if(strcmp(keyword, "change") == 0){
-            mudarCelula(&terminal1);
-
-            quickSort(terminal1.celulaPrior, 0, terminal1.tam-1, true);
-            quickSort(terminal1.celulaTempo, 0, terminal1.tam-1, false);
+            mudarCelula(&terminal, &bestPrior, &bestTempo);
         }
         
-        // Comando "pinto"
-        // HAaha muito engraçado dantovisk
+        // Comando "print"
         else if(strcmp(keyword, "print") == 0){
-            if(quickViavel){
-                quickSort(terminal1.celulaPrior, 0, terminal1.tam-1, true);
-                quickSort(terminal1.celulaTempo, 0, terminal1.tam-1, false);
-                quickViavel = false;
-            }
-
             char key;
             scanf(" -%c", &key);
-
-            if(key == 'p')
-                printarProcessos(terminal1.celulaPrior, terminal1.tam);
-            else if(key == 't')
-                printarProcessos(terminal1.celulaTempo, terminal1.tam);
+            if(key == 'p') {
+                quickSort(terminal.celulaPrior, 0, terminal.tam-1, true);
+                printarProcessos(terminal.celulaPrior, terminal.tam);
+            } else if(key == 't') {
+                quickSort(terminal.celulaTempo, 0, terminal.tam-1, false);
+                printarProcessos(terminal.celulaTempo, terminal.tam);
+            }
         }
 
     } while(strcmp(keyword, "quit") != 0);
 
-    for(int i = 0; i < terminal1.tam; i++){
-        free(terminal1.celulaPrior[i]);
-        terminal1.celulaPrior[i] = NULL;
+    for(int i = 0; i < terminal.tam; i++){
+        free(terminal.celulaPrior[i]);
+        terminal.celulaPrior[i] = NULL;
     }
 
     return 0;
